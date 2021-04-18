734bc0d0d4648e3057c3b5937e6a06d43ba97bd6
Harsh Sharma <68972644+Codelif@users.noreply.github.com>
2021-04-18 14:46:14 +0530

    time step

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 test = """2H2 + O2 -> 2H2O"""
 
-def times_sep(ele):
-    lst = []
-    for i in ele:
-        times = ""
-        if i == i.lower():
-            pass
-        elif i.isdigit():
-            times+=i
-        # elif 
+def times_sep(s):
+    elem = []
+    times = []
+    checking_pro = False
+    last = 0
+    for i in range(len(s)):
+        if checking_pro:        
+            if s[i].isalpha() and not s[i].islower() or s[i].isdigit():
+                elem.append(s[last])
+            checking_pro = False
+        if s[i].islower() and s[i].isalpha():
+            elem.append(s[i-1]+s[i])
+        if s[i].isupper():
+            checking_pro = True
+        if s[i].isupper() and i == len(s)-1:
+            elem.append(s[i])
+
+        last = (i)
+    last = 0
+    checking_pro = False
+    for i in range(1, len(s)):
+        if checking_pro:
+            if s[i].isupper():
+                times.append(1)            
+            checking_pro = False
+        if s[i].isdigit():times.append(int(s[i]))
+        if s[i].isalpha():
+            checking_pro = True
+    return elem, times
+    
 def coeff_sep(ele):
     d = {}
     for i in ele:
---
+++ times_sep.py
@@ -0,0 +1,6 @@
+s = "Ye2Nu3U3JIG2B2Lo3M8"
+
+        
+
+
+
